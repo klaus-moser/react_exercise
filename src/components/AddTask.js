0b82a5d1cc97e190d4c6701f729a310a85084f05
klaus-moser <klaus-moser@posteo.de>
2022-04-20 17:55:05 +0200

    EODC

---
+++ src/components/AddTask.js
@@ -0,0 +1,17 @@
+const AddTask = () => {
+  return (
+    <form className="add-form">
+      <div className="form-control">
+        <label>Task</label>
+        <inpu type='text' placeholder='Add Task'></inpu>
+      </div>
+    </form>
+  )
+}
+
+
+export default AddTask
+
+
+https://www.youtube.com/watch?v=w7ejDZ8SWv8
+1:04:45
