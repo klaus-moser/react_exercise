e7896914f16fbbbb722108b01cbcfc19895d6e1c
klaus-moser <klaus-moser@posteo.de>
2022-04-21 21:55:03 +0200

    Fetch data from json-server

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import Header from "./components/Header";
 import Tasks from './components/Tasks';
 import AddTask from "./components/AddTask";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState(
-    [])
+  const [tasks, setTasks] = useState([])
+
+  
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  // Fetch content from json-server
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
   
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
